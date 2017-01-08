ae9fac14e1a96d25740c71081a04358050945352
Brian Rutledge <bhrutledge@gmail.com>
2017-01-08 13:09:24 -0500

    Improve Bash completion

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -1,10 +1,8 @@
 # http://stackoverflow.com/questions/1676426/how-to-check-the-ls-version
 if ls --color -d . >/dev/null 2>&1; then
     colorflag='--color'
-    # TODO: export LS_COLORS
 elif ls -G -d . >/dev/null 2>&1; then
     colorflag='-G'
-    # TODO: export LSCOLORS
 fi
 
 alias ls="command ls -h $colorflag"
--- bash/exports.bash
+++ bash/exports.bash
@@ -51,3 +51,7 @@ export LESS_TERMCAP_mb=$(tput dim)$red
 export LESS_TERMCAP_md=$(tput bold)$red
 export LESS_TERMCAP_us=$(tput bold)$green
 export LESS_TERMCAP_ue=$reset
+
+# http://geoff.greer.fm/lscolors/
+export LSCOLORS='exfxcxdxbxegedabagacad'
+export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43'
