5236f7c344f07ed89c8e47bc6d743efaaa672a87
Brian Rutledge <brian@bhrutledge.com>
2020-01-03 09:21:33 -0500

    Don't write history immediately

--- src/.bash/history.sh
+++ src/.bash/history.sh
@@ -6,6 +6,8 @@ HISTIGNORE='[ ]*:exit:ls*:history*'
 shopt -s cmdhist
 shopt -s histappend
 
-# Save commands immediately, instead of end of session
-# Add "; history -c; history -r" to sync between sessions
-PROMPT_COMMAND+="; history -a"
+# Save commands immediately, instead of end of session, to avoid lost history
+# This will interleave commands from multiple sessions
+# PROMPT_COMMAND+="; history -a"
+# Sync history between active sessions
+# PROMPT_COMMAND+="; history -c; history -r"
